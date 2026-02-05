a4373c4faf8317989c99069cb338fefe601e99b3
Dmitriy L <dukat2007@gmail.com>
2026-02-06 06:49:38 +0700

    Configure GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+    base: process.env.VITE_BASE_URL || '/',
     root: 'src/app',
     publicDir: '../../public',
     build: {
